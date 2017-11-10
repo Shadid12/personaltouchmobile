39efe7ea229e3dccabe327e6f235d3c5013e2dea
shadid12 <shadidhaque2012@gmail.com>
2017-11-10 10:52:57 -0500

    working prototype

---
+++ App.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import { StyleSheet,
+    Text,
+    TextInput,
+    View,
+    Image,
+    TouchableOpacity,
+    KeyboardAvoidingView }
+    from 'react-native';
+import BarcodeScaner from './components/BarcodeScaner';
+
+export default class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            logged: false
+        }
+    }
+
+    handleLoging = (e) => {
+        if (this.state.username) {
+            this.setState({logged: true});
+        }
+    };
+
+
+    notLoggedin() {
+        return (
+            <KeyboardAvoidingView behavior="padding" style={styles.container}>
+                <View style={styles.logo}>
+                    <Image
+                        source={require('./img/box.png')}
+                    />
+                    <Text>Username is {this.state.username}</Text>
+                </View>
+                <View style={styles.form}>
+                    <View style={stylesForm.container}>
+                        <TextInput
+                            placeholder="username"
+                            style={stylesForm.input}
+                            onChangeText={(text) => this.setState({username: text})}
+                            value={this.state.username}
+                        />
+                        <TouchableOpacity style={stylesForm.buttonContainer}
+                                          onPress={this.handleLoging}
+                        >
+                            <Text style={stylesForm.buttonText} >Login</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </KeyboardAvoidingView>
+        )
+    }
+
+
+    render() {
+        if (this.state.logged === false) {
+            return (this.notLoggedin())
+        } else {
+            return (
+                <BarcodeScaner
+                    name={this.state.username}
+                />
+            )
+        }
+  }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#C1CAD6'
+    },
+    logo: {
+        alignItems: 'center',
+        flexGrow: 1,
+        justifyContent: 'center'
+    },
+    form: {
+
+    }
+});
+
+const stylesForm = StyleSheet.create({
+    container: {
+        padding: 20
+    },
+
+    input: {
+        height: 40,
+        backgroundColor: 'rgba(255,255,255,0.2)',
+        marginBottom: 80
+    },
+
+    buttonContainer: {
+        backgroundColor: '#856084',
+        paddingVertical: 20
+    },
+
+    buttonText: {
+        textAlign: 'center',
+        color: '#FFF'
+    }
+
+});
---
+++ components/BarcodeScaner.js
@@ -0,0 +1,123 @@
+import React, { Component } from 'react';
+import { Text,
+    StyleSheet,
+    View,
+    Alert,
+    TouchableOpacity}
+    from 'react-native';
+import { BarCodeScanner, Permissions } from 'expo';
+import axios from 'axios';
+
+export default class BarcodeScaner extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            hasCameraPermission: null,
+        }
+    }
+
+    async componentWillMount() {
+        const { status } = await Permissions.askAsync(Permissions.CAMERA);
+        this.setState({hasCameraPermission: status === 'granted'});
+    }
+
+    render() {
+        const { hasCameraPermission } = this.state;
+        if (hasCameraPermission === null) {
+            return <Text>Requesting for camera permission</Text>;
+        } else if (hasCameraPermission === false) {
+            return <Text>No access to camera</Text>;
+        } else {
+            return (
+                <View style={{ flex: 1 }}>
+                    <BarCodeScanner
+                        onBarCodeRead={this._handleBarCodeRead}
+                        style={styles.barCode}
+                    />
+                    <TouchableOpacity
+                        style={styles.showCount}
+                        onPress={this._showCount}
+                    >
+                        <Text style={styles.showCountText}>View Count</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.refresh}
+                        onPress={() => {
+                            this.setState({hasCameraPermission: null}, () => {
+                                this.setState({hasCameraPermission: true},() => {
+                                    console.log('refreshed');
+                                })
+                            });
+                        }}
+                    >
+                        <Text style={styles.showCountText}>Refresh</Text>
+                    </TouchableOpacity>
+                </View>
+            )
+        }
+    }
+
+    _showCount = () => {
+      console.log('Not implemented yet !!');
+    };
+
+    _handleBarCodeRead = ({ type, data }) => {
+        // alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+        // console.log(data);
+        var r = '';
+        var ro2 = data.split('R02~');
+        var ro3 = data.split('R03~');
+        var ro4 = data.split('R04~');
+        var ro5 = data.split('R05~');
+        var ro6 = data.split('R06~');
+        var ro7 = data.split('R07~');
+        r = r + ro2[1].split('|')[0] + ' ' +  ro3[1].split('|')[0] + ' ' + ro4[1].split('|')[0] + ' ' + ro5[1].split('|')[0]
+            + ' ' + ro6[1].split('|')[0] + ' ' + ro7[1].split('|')[0];
+        r = r.replace(/\s\s+/g, ' ');
+        var so2 = data.split('S02~');
+        var pin = so2[1].split('|')[0];
+        Alert.alert(
+            `Barcode Scanned`,
+            `Address: ${r} Pin: ${pin}`,
+            [
+                {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+                {text: 'Send', onPress: () => {
+                    axios.post('http://shadid12.herokuapp.com/item', {
+                        username: this.props.name,
+                        address:  r,
+                        pin: pin
+                    }).then((res) => {
+                        console.log(res);
+                    })
+                }},
+            ],
+            { cancelable: false }
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#C1CAD6'
+    },
+    barCode: {
+        height: 500,
+        width: 500,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    showCount: {
+        backgroundColor: '#C1CAD6',
+        padding: 10
+    },
+    refresh: {
+        backgroundColor: '#38d603',
+        padding: 10
+    },
+    showCountText: {
+        textAlign: 'center',
+        color: '#FFF'
+    }
+});
